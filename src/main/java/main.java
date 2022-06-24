b0cc09e58ae5807d55fc02bbb69ae7846c56ac4b
Konstantin <smykovkn@gmail.com>
2022-06-24 23:05:05 +0700

    Done task

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
-
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            if (!scanner.hasNextLine()) {
+                return;
+            }
+            String input = scanner.nextLine();
+            String[] arr_str = input.split("\\p{Blank}+", 2);
+            int sum = 0;
+            if ("".equals(arr_str[0])) {
+                return;
+            }
+            if (arr_str.length == 2) {
+                sum = Integer.parseInt(arr_str[0]) + Integer.parseInt(arr_str[1]);
+                System.out.println(sum);
+            }
+        }
     }
 }
